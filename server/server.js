78d3289a94540ec975e4994a578ed040511bb952
Tobias Zucali <tzuc@usersnap.com>
2017-02-14 15:46:52 +0100

    Make body template optional

--- server/server.js
+++ server/server.js
@@ -5,11 +5,23 @@ var webServer = express();
 var handlebars = require('handlebars');
 var jsonfile = require('jsonfile')
 
-config = {
+var config = {
     "jsonFile": "",
     "bodyFile": "",
     "contentFile": ""
 };
+var defaultBodyTemplate = `<!DOCTYPE html>
+  <html>
+   <head>
+     <title>Mandrill handlebars helper</title>
+     <style>
+       /* some global styles */
+     </style>
+   </head>
+   <body>
+     {{> content}}
+   </body>
+  </html>`;
 
 webServer.use(express.static('server/static'));
 webServer.use(bodyParser.json());
@@ -17,7 +29,7 @@ webServer.use(bodyParser.urlencoded({extended: false}));
 webServer.listen(20755);
 webServer.get('/', function (req, res) {
     var contentSource = fs.readFileSync(config.contentFile).toString();
-    var bodySource = fs.readFileSync(config.bodyFile).toString();
+    var bodySource = (config.bodyFile) ? fs.readFileSync(config.bodyFile).toString() : defaultBodyTemplate;
     handlebars.registerPartial('content', contentSource);
     var template = handlebars.compile(bodySource);
     res.send(template(jsonfile.readFileSync(config.jsonFile)));
